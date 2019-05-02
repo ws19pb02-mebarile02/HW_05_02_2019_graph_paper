e590c780e41a4418258960bef727fe4f3681562e
Michael Barile <33465350+mebarile02@users.noreply.github.com>
2019-05-02 12:21:36 -0400

    Add files via upload

---
+++ graph_paper.py
@@ -0,0 +1,29 @@
+"""
+This program prints graph paper to user specifications.
+"""
+
+
+import sys
+
+rows_boxes = int(input("How many rows of boxes? "))
+columns_boxes = int(input("How many columns of boxes? "))
+rows_spaces = int(input("How many rows of spaces in each box? "))
+columns_spaces = int(input("How many columns of spaces in each box? "))
+
+
+
+for i in range(rows_boxes):
+
+    for j in range(columns_boxes):
+        print("+" + columns_spaces*"-", end = "")
+
+    print('+')
+
+    for h in range(rows_spaces):
+        print(columns_boxes*("|" + columns_spaces*" "), end = "")
+        print('|')
+       
+print(columns_boxes*("+" + columns_spaces*"-"), end = "")
+print('+')
+
+sys.exit(0)
